a89f77a7d81d67666082635e56d0f19ed4500e90
SHEHZAD <99957366+muhammadshehzad038@users.noreply.github.com>
2022-10-22 21:07:05 +0500

    Add files via upload

---
+++ lab4 task1.py
@@ -0,0 +1,97 @@
+from typing import List, Any
+
+
+class Node:
+    def __init__(self, state, parent, actions, totalcost):
+        self.state = state
+        self.parent = parent
+        self.actions = actions
+        self.totalcost = totalcost
+
+        graph = {
+    'Arad': ['Sibiu', 'Zerind', 'Timisoara'],
+    'Zerind': ['Arad', 'Oradea'],
+    'Oradea': ['Zerind', 'Sibiu'],
+    'Sibiu': ['Arad', 'Oradea', 'Fagaras', 'Rimnicu'],
+    'Timisoara': ['Arad', 'Lugoj'],
+    'Lugoj': ['Timisoara', 'Mehadia'],
+    'Mehadia': ['Lugoj', 'Drobeta'],
+    'Drobeta': ['Mehadia', 'Craiova'],
+    'Craiova': ['Drobeta', 'Rimnicu', 'Pitesti'],
+    'Rimnicu': ['Sibiu', 'Craiova', 'Pitesti'],
+    'Fagaras': ['Sibiu', 'Bucharest'],
+    'Pitesti': ['Rimnicu', 'Craiova', 'Bucharest'],
+    'Bucharest': ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
+    'Giurgiu': ['Bucharest'],
+    'Urziceni': ['Bucharest', 'Vaslui', 'Hirsova'],
+    'Hirsova': ['Urziceni', 'Eforie'],
+    'Eforie': ['Hirsova'],
+    'Vaslui': ['Iasi', 'Urziceni'],
+    'Iasi': ['Vaslui', 'Neamt'],
+    'Neamt': ['Iasi']
+}
+
+
+
+
+
+def DFS():
+    initialstate = 'Arad'
+    goalState = 'Bucharest'
+
+    graph = {
+    'Arad': ['Sibiu', 'Zerind', 'Timisoara'],
+    'Zerind': ['Arad', 'Oradea'],
+    'Oradea': ['Zerind', 'Sibiu'],
+    'Sibiu': ['Arad', 'Oradea', 'Fagaras', 'Rimnicu'],
+    'Timisoara': ['Arad', 'Lugoj'],
+    'Lugoj': ['Timisoara', 'Mehadia'],
+    'Mehadia': ['Lugoj', 'Drobeta'],
+    'Drobeta': ['Mehadia', 'Craiova'],
+    'Craiova': ['Drobeta', 'Rimnicu', 'Pitesti'],
+    'Rimnicu': ['Sibiu', 'Craiova', 'Pitesti'],
+    'Fagaras': ['Sibiu', 'Bucharest'],
+    'Pitesti': ['Rimnicu', 'Craiova', 'Bucharest'],
+    'Bucharest': ['Fagaras', 'Pitesti', 'Giurgiu', 'Urziceni'],
+    'Giurgiu': ['Bucharest'],
+    'Urziceni': ['Bucharest', 'Vaslui', 'Hirsova'],
+    'Hirsova': ['Urziceni', 'Eforie'],
+    'Eforie': ['Hirsova'],
+    'Vaslui': ['Iasi', 'Urziceni'],
+    'Iasi': ['Vaslui', 'Neamt'],
+    'Neamt': ['Iasi']
+}
+
+
+
+    frontier = [initialstate]
+    explored = []
+    while len(frontier) != 0:
+        currentNode = frontier.pop(len(frontier) - 1)
+        print(currentNode)
+        explored.append(currentNode)
+        currentChildren = 0
+        for child in graph[currentNode].actions:
+            if child not in frontier and child not in explored:
+                graph[child].parent = currentNode
+                if graph[child].state == goalState:
+                    print(explored)
+                    return actionsequence(graph, initialstate, goalState)
+                currentChildren = currentChildren + 1
+                frontier.append(child)
+        if currentChildren == 0:
+            del explored[len(explored) - 1]
+
+
+def actionsequence(graph, initialstate, goalState):
+    sol = [goalState]
+    currentparent = graph[goalState].parent
+    while currentparent is not None:
+        sol.append(currentparent)
+        currentparent = graph[currentparent].parent
+    sol.reverse()
+    return sol
+
+
+sol = DFS()
+print(sol)
